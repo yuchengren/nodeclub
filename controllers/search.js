c0b777065e2d8b5fa3c762bc1ad56e80c2ccdea1
yuchengren <513082359@qq.com>
2021-01-20 18:31:58 +0800

    谷歌搜索，加入自己的域名

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,7 @@
+var config = require('../config');
+
 exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
-  res.redirect('https://www.google.com.hk/search?q=site:cnodejs.org+' + q);
+  res.redirect('https://www.google.com.hk/search?q=site:'+ config.host + "+"  + q);
 };
